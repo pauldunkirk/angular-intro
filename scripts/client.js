928d1a361ced7e7add9ff901ab3297b64e9a0c61
Paul Dunkirk <pauldunkirk@gmail.com>
2017-03-06 18:27:12 -0600

    base mode

--- scripts/client.js
+++ scripts/client.js
@@ -1,24 +1,148 @@
 var phirePhiterApp = angular.module('PhirePhiterApp', []);
+phirePhiterApp.controller('PhirePhiterController', function() {
+    console.log('PhirePhiter controller loaded');
+    var self = this; // holds the value of this
+    self.people = [
+        {
+            "firstName": "Arrielle",
+            "lastName": "Kooiman",
+            "git_username": "Arrielle",
+            "shoutout": "Y'all have been PHInomenal",
+            "imageURL": "https://avatars2.githubusercontent.com/u/23461237?v=3&s=460"
+        },
 
-phirePhiterApp.controller('PhirePhiterController', function(){
-  console.log('PhirePhiter controller loaded');
+        {
+            "firstName": "Melissa",
+            "lastName": "Hare",
+            "git_username": "Melissa-Elizabeth",
+            "shoutout": "Hi",
+            "imageURL": "https://avatars1.githubusercontent.com/u/23161008?v=3&s=460"
+        },
+        {
+            "firstName": "Chad",
+            "lastName": "Sundberg",
+            "git_username": "chadsundberg",
+            "shoutout": "Celina!!  She taught me how to use two fingers to scroll in my open windows!  Oh, and I'd like to thank var taco!",
+            "imageURL": "https://avatars3.githubusercontent.com/u/23482083?v=3&s=460"
+        },
+        {
+            "firstName": "Christine",
+              "lastName": "Pogatchnik",
+            "git_username": "kikipogo",
+            "shoutout": "Everyone",
+            "imageURL": "https://avatars0.githubusercontent.com/u/16141654?v=3&s=460"
+        },
+        {
+            "firstName": "Thomas",
+              "lastName": "Johander",
+            "git_username": "lopan455",
+            "shoutout": "Yo! Thanks for that!",
+            "imageURL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/7/005/095/33c/07b993b.jpg"
+        },
+        {
+            "firstName": "Edmund",
+              "lastName": "Wippler",
+            "git_username": "edwippler",
+            "shoutout": "Sup?",
+            "imageURL": "https://media.licdn.com/media/AAEAAQAAAAAAAAeoAAAAJGM5NWY3NmYzLWNiZTQtNDRmYy05YTNmLTQxZDJkOTk0M2QzMA.jpg"
+        },
+        {
+            "nafirstNameme": "Jonny",
+              "lastName": "Holupchinski",
+            "git_username": "jonholup",
+            "shoutout": "Yo!",
+            "imageURL": "http://animal-dream.com/data_images/tiger/tiger8.jpg"
+        },
+        {
+            "firstName": "Catherine",
+              "lastName": "Jarocki",
+            "git_username": "cjaro",
+            "shoutout": "Thank you to the internet of resources!",
+            "imageURL": "https://avatars1.githubusercontent.com/u/19612689?v=3&s=460"
 
-  var self = this; // holds the value of this
+        },
+        {
+            "firstName": "Kris",
+              "lastName": "Jensen",
+            "git_username": "kristinejensen",
+            "shoutout": "Thank you to Luke, Kris, and Chris for being great instructors!",
+            "imageURL": "https://media.licdn.com/media/p/2/005/058/1c9/36c8207.jpg"
+        },
+        {
+            "firstName": "Paul",
+              "lastName": "Dunkirk",
+            "git_username": "pauldunkirk",
+            "shoutout": "Phi peops rock!",
+            "imageURL": "https://avatars1.githubusercontent.com/u/19807093?v=3&s=460"
+        },
+        {
+            "firstName": "Celina",
+              "lastName": "Peterson",
+            "git_username": "celina",
+            "shoutout": "Peace out!",
+            "imageURL": "https://avatars1.githubusercontent.com/u/58040?v=3&s=460"
+        },
+        {
+            "firstName": "Chris",
+              "lastName": "Stanton",
+            "git_username": "chris-stanton",
+            "shoutout": "Peace",
+            "imageURL": "http://tcbmag.com/getattachment/037be106-8f16-4dff-bc40-81462e91d177"
+        },
+        {
+            "firstName": "Philip",
+              "lastName": "Curtis",
+            "git_username": "PCSailor",
+            "shoutout": "Thanks to Luke and Kris, and Chris, Scott, & Fred",
+            "imageURL": "https://avatars2.githubusercontent.com/u/22984498?v=3&s=460"
+        },
+        {
+            "firstName": "Sam",
+              "lastName": "Velie",
+            "git_username": "samvelie",
+            "shoutout": "Love y'all ",
+            "imageURL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAfwAAAAJGQxMjcyNDcxLTJkYzktNDFjMC1hYzQ1LTBiMWU3NmNmYjRhOA.jpg"
+        },
+        {
+            "firstName": "Paige",
+              "lastName": "Bystrom",
+            "git_username": "Radmint",
+            "shoutout": "Thanks to Phi cohort for being awesome, positive people! ",
+            "imageURL": "https://avatars3.githubusercontent.com/u/23347804?v=3&s=460"
+        },
+        {
+            "firstName": "Jeremy",
+              "lastName": "Leif",
+            "git_username": "JL-golf14",
+            "shoutout": "break down the Phirewalls of doubt!!!",
+            "imageURL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAdZAAAAJGQ1NmE1ODdhLTRhZWQtNGM3MS05NzViLTljODdhNDZmNGFmNA.jpg"
+        },
+        {
+            "firstName": "Kris",
+              "lastName": "Szafranski",
+            "git_username": "kdszafranski",
+            "shoutout": "Thank you to Phi for your open, warm, and thoughtful speeches. You rekindle my faith in humanity!",
+            "imageURL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAANLAAAAJGVmOTljZDA5LTk0NWUtNDlmOC1hNzg0LWViOWU4NjZjMTc3Ng.jpg"
+        },
+        {
+            "firstName": "Luke",
+              "lastName": "Schlangen",
+            "git_username": "lukeschlangen",
+            "shoutout": "Thanks for being awesome during the angular lecture today.",
+            "imageURL": "https://media.licdn.com/media/AAEAAQAAAAAAAAdiAAAAJDk0MWZkMWNlLTJiNTktNGMzYi1hYjk3LTUzNzJkNWJjMTRkYg.jpg"
+        }
+];
 
-  self.people = [
-    {
-      "firstName": "Luke",
-      "lastName": "Schlangen",
-      "git_username": "LukeSchlangen",
-      "shoutout": "Thanks for being awesome during the angular lecture today."
-    }
-  ];
+self.newPerson = {};
 
-  self.newPerson = {};
-
-  self.createPerson = function(){
+self.createPerson = function() {
     console.log(self.newPerson);
     self.people.push(angular.copy(self.newPerson));
-  };
+};
 
 });
+
+
+{
+
+}
